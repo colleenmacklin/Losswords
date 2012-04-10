66f0411f35a04ad985ab2499ea62c808a3f925da
colleenmacklin <colleenmacklin@gmail.com>
2012-04-10 13:47:27 -0400

    touch to highlight

---
+++ src/Scrollbar.cpp
@@ -0,0 +1,9 @@
+//
+//  Scrollbar.cpp
+//  Losswords_1
+//
+//  Created by Colleen Macklin on 4/10/12.
+//  Copyright (c) 2012 Parsons School of Design. All rights reserved.
+//
+
+#include <iostream>
---
+++ src/Scrollbar.h
@@ -0,0 +1,14 @@
+//
+//  Scrollbar.h
+//  Losswords_1
+//
+//  Created by Colleen Macklin on 4/10/12.
+//  Copyright (c) 2012 Parsons School of Design. All rights reserved.
+//
+
+#ifndef Losswords_1_Scrollbar_h
+#define Losswords_1_Scrollbar_h
+
+
+
+#endif
